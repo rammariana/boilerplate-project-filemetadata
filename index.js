75f0f358f9014ebe3204ec22ef72c56539bbd0e7
Mariana Ramírez <marianasmatos1@gmail.com>
2023-11-30 21:22:44 -0500

    New Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+require('dotenv').config();
+const multer = require('multer');
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+const upload = multer({ dest: './public/data/uploads' });
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size,
+  });
+  console.log(req.file);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log('Your app is listening on port ' + port);
 });
